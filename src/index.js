c626aef04a45562fdab6d7ed1db80609cc1026d5
rako <69637576+rako@users.noreply.github.com>
2025-09-20 00:36:50 +0900

    カウンターの作成とKVストレージのアタッチを行った

--- src/index.js
+++ src/index.js
@@ -10,6 +10,38 @@
 
 export default {
 	async fetch(request, env, ctx) {
-		return new Response('Hello World!');
+		try {
+			const url = new URL(request.url);
+
+			// ここで、favicon.icoを読み込んでしまうとリクエストが2回ある事になってしまい、counterが2回増えてしまう
+			if (url.pathname === '/favicon.ico') {
+				return
+			};
+
+			let counter = await env.ACCESS_COUNTER.get("counter");
+			counter = Number(counter);
+
+			if (counter === null) {
+				return new Response("Counter not found", { status: 404 })
+			};
+
+			counter = counter + 1;
+
+			const welcomeMessage = `${counter}回目のお客さんです！`;
+
+			await env.ACCESS_COUNTER.put("counter", `${counter.toString()}`);
+
+			return new Response(welcomeMessage);
+		} catch (err) {
+			console.error(`KV returned error:`, err);
+			const errorMessage =
+				err instanceof Error
+					? err.message
+					: "An unknown error occurred when accessing KV storage";
+			return new Response(errorMessage, {
+				status: 500,
+				headers: { "Content-type": "text/plain" },
+			});
+		}
 	},
 };
